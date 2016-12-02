e6b5d9de32aa0545f454d6829bb9792bc5a323b5
arlieu <avelieu@gmail.com>
2016-12-02 02:32:05 -0500

    First commit
    First Commit

---
+++ merge-sort.py
@@ -0,0 +1,44 @@
+def merge(lower, upper, array):
+    nL = len(lower)
+    nU = len(upper)
+
+    i = j = k = 0
+    while i<nL and j<nU:
+        if lower[i]<upper[j]:
+            array[k] = lower[i]
+            i+=1
+        else:
+            array[k] = upper[j]
+            j+=1
+        k+=1
+
+    while i<nL:
+        array[k] = lower[i]
+        i+=1
+        k+=1
+
+    while j<nU:
+        array[k] = upper[j]
+        j+=1
+        k+=1
+    return array
+
+def mergeSort(array):
+    length = len(array)
+    if length < 2:
+        return
+
+    mid = length//2
+
+    l = [x for x in array[:mid]]
+    u = [x for x in array[mid:]]
+
+    mergeSort(l)
+    mergeSort(u)
+    res = merge(l, u, array)
+    return res
+
+
+
+test = [1, 3, -4, 5, 6, 3,0]
+print(mergeSort(test))

3169ea9ef853ad97059278717f7bb9949d0f4075
Avery Lieu <avelieu@gmail.com>
2016-12-11 16:37:46 -0500

    counting sort, radix sort in progress

---
+++ algorithms/sorting/distributed/counting-sort.py
@@ -0,0 +1,20 @@
+def countSort(array):
+    upper = max(array)
+    lower = min(array)
+
+    countArr = [0]*(upper+1)
+
+    for i in array:
+        countArr[i] += 1
+
+    for i in range(1, len(countArr)):
+        countArr[i] += countArr[i-1]
+
+    res = [None]*len(array)
+
+    for i in array:
+        index = countArr[i]
+        countArr[i] -= 1
+        res[index-1] = i
+
+    return res
